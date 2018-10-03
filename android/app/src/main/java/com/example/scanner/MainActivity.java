8e3272f37434b14731aa8283cb073600de1bdcf7
Marc Stroebel <m.stroebel@gmx.de>
2018-10-03 09:47:36 +0200

    initial

---
+++ android/app/src/main/java/com/example/scanner/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.scanner;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+
+    private static final String CHANNEL = "samples.flutter.io/battery";
+
+    public MainActivity() {
+        super();
+        System.loadLibrary("opencv_java3");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                        // TODO
+                    }
+                });
+    }
+}
